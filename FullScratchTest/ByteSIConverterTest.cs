29a1709066e1e33391b3fd0060ba61954dafa991
SHIBAURA\H0882 <t_iwasaki@shibaura-e.co.jp>
2020-07-19 23:37:21 +0900

    Test add

---
+++ FullScratchTest/ByteSIConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FullScratch.Models;
+
+namespace FullScratchTest
+{
+    [TestClass]
+    public class ByteSIConverterTest
+    {
+        [TestMethod]
+        public void ByteにSI接頭辞を付加できる()
+        {
+            var BC = new ByteSIConverter();
+            Assert.AreEqual("1023B", BC.Convert((long)1023,typeof(long),null,null));
+            Assert.AreEqual("1KB", BC.Convert((long)1024, typeof(long), null, null));
+            Assert.AreEqual("1MB", BC.Convert((long)1048576, typeof(long), null, null));
+            Assert.AreEqual("1GB", BC.Convert((long)1073741824, typeof(long), null, null));
+            Assert.AreEqual("1TB", BC.Convert((long)1099511627776, typeof(long), null, null));
+        }
+    }
+}
---
+++ FullScratchTest/MenuTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FullScratch.Models;
+using FullScratch.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FullScratchTest
+{
+    [TestClass]
+    public class MenuTest
+    {
+        [TestMethod]
+        public void Menuインスタンスを取得できる()
+        {
+        }
+    }
+}
